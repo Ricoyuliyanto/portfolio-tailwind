ae6516561d25493475520c5285bc8dc9cb75e61f
Ricoyuliyanto <echo.ricoyulyanto16@gmail.com>
2022-11-02 18:28:50 +0700

    update perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 };
 
@@ -19,3 +24,13 @@ humburger.addEventListener('click', function() {
     humburger.classList.toggle('humburger-active');
     navMenu.classList.toggle('hidden');
 }); 
+
+
+
+// klick diluar humberger
+window.addEventListener('click', function (e) {
+    if(e.target != humburger && e.target != navMenu) {   
+        humburger.classList.remove('humburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
